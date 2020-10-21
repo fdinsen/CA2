a2ff0676449198bca18f080fc3fd1f4807da9dde
Frederik <gammarik@gmail.com>
2020-10-21 08:48:54 +0200

    add test saved

--- src/test/java/facades/PersonFacadeTest.java
+++ src/test/java/facades/PersonFacadeTest.java
@@ -1,9 +1,9 @@
 package facades;
 
-import dto.PersonDTO;
-import entities.Address;
 import entities.Cityinfo;
+import entities.Address;
 import entities.Person;
+import dto.PersonDTO;
 import utils.EMF_Creator;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -20,12 +20,12 @@ public class PersonFacadeTest {
 
     private static EntityManagerFactory emf;
     private static PersonFacade facade;
-    
+
     Person p1;
     Person p2;
     Person p3;
     Person p4;
-    
+
     Address a1;
 
     public PersonFacadeTest() {
@@ -33,8 +33,8 @@ public class PersonFacadeTest {
 
     @BeforeAll
     public static void setUpClass() {
-       emf = EMF_Creator.createEntityManagerFactoryForTest();
-       facade = PersonFacade.getPersonFacade(emf);
+        emf = EMF_Creator.createEntityManagerFactoryForTest();
+        facade = PersonFacade.getPersonFacade(emf);
     }
 
     @AfterAll
@@ -47,20 +47,20 @@ public class PersonFacadeTest {
     @BeforeEach
     public void setUp() {
         EntityManager em = emf.createEntityManager();
-        
+
         p1 = new Person(12345678, "mail", "navn", "andetNavn");
         p2 = new Person(23456789, "mail1", "navn1", "andetNavn1");
         p3 = new Person(34567890, "mail2", "navn2", "andetNavn2");
         p4 = new Person(45678901, "mail3", "navn3", "andetNavn3");
-        
+
         Address a1 = new Address(1, "vej vej");
         a1.setAdditionalInfo("");
         a1.setZipcode(new Cityinfo("3400", "Hillerod"));
         p1.setAddress(a1);
-        
+
         try {
             em.getTransaction().begin();
-            
+
             em.createNamedQuery("Person.deleteFrom").executeUpdate();
             em.persist(p1);
             em.persist(p2);
@@ -77,42 +77,41 @@ public class PersonFacadeTest {
     public void tearDown() {
 //        Remove any data after each test was run
     }
-    
+
     @Test
-    public void getPersonByPhone(){
-        
+    public void getPersonByPhone() {
+
         String expected = p1.getFirstName();
-        
+
         PersonDTO p = facade.getPersonByPhone(p1.getPhone());
-        
+
         String actual = p.getFirstName();
-        
+
         assertEquals(expected, actual);
-                
+
     }
-    
-    
+
     public void testCreatePerson() {
         //Arrange
+        EntityManager em = emf.createEntityManager();
+        int phone = 12345678;
         Person personToCreate = new Person(
-            12345678, "email@email.com",
-            "Peter", "Petersen");
+                phone, "email@email.com",
+                "Peter", "Petersen");
         Address addressToCreate = new Address(
-            "Espegaardsvej 20");
-        Cityinfo city = new Cityinfo(
-            "2860");
+                "Espegaardsvej 20");
+        Cityinfo city = em.find(Cityinfo.class, "2860");
         addressToCreate.setZipcode(city);
         personToCreate.setAddress(addressToCreate);
-        PersonDTO dto = PersonDTO(personToCreate);
-        
+        PersonDTO dto = new PersonDTO(personToCreate);
+
         //Act
         facade.createPerson(dto);
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        
-        //Assert
-        
-    }
-        
 
+        //PersonDTO actual = facade.getPersonByPhone(phone);
+        Person actual = em.find(Person.class, phone);
+
+        //Assert
+        assertEquals(dto.getEmail(), actual.getEmail());
+    }
 }
